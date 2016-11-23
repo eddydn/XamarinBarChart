ecfee69e9734f33f162a8a14d38ab646c08f7419
eddydn <eddydn@gmail.com>
2016-11-23 13:32:19 +0700

    First Commit
    First Commit

---
+++ MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using BarChart;
+using System;
+using Android.Views;
+using Android.Support.V7.App;
+
+namespace XamarinBarChart
+{
+    [Activity(Label = "XamarinBarChart", MainLauncher = true, Icon = "@drawable/icon",Theme ="@style/Theme.AppCompat.Light.NoActionBar")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            var data = new[] {10f,20f,100f,50f,20f };
+            var chart = new BarChartView(this)
+            {
+                ItemsSource = Array.ConvertAll(data,v=>new BarModel { Value = v})
+            };
+
+            AddContentView(chart, new ViewGroup.LayoutParams(
+                WindowManagerLayoutParams.MatchParent, 
+                WindowManagerLayoutParams.MatchParent));
+        }
+    }
+}
+
